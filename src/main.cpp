2482274a188ae951bc550707c43c70294d8fd04f
Hammad Majid <hammadmajid@proton.me>
2023-12-29 00:33:24 +0500

    feat: let user choice html file path and name

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 #include "generator.h"
 
 int main(const int argc, char *argv[]) {
-    std::string file_path = process_args(argc, argv);
+    Args args = process_args(argc, argv);
 
-    if(!validate_file(file_path)) {
-        std::cout << file_path << " is not a valid file." << std::endl;
+    if(!validate_file(args.input_file)) {
+        std::cout << args.input_file << " is not a valid file." << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    const Lexer lexer(file_path);
+    const Lexer lexer(args.input_file);
     const auto tokens = lexer.tokenize();
 
     Generator generator(tokens);
@@ -19,7 +19,7 @@ int main(const int argc, char *argv[]) {
 
     std::cout << content << std::endl;
 
-    create_html_file(content, file_path);
+    create_html_file(content, args.output_file);
 
     return EXIT_SUCCESS;
 }
