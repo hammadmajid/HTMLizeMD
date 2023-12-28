d699b733307261a08033e81835a44713c892587a
Hammad Majid <hammadmajid@proton.me>
2023-12-29 00:38:44 +0500

    refactor: convert to structured binding of Args

--- src/main.cpp
+++ src/main.cpp
@@ -4,14 +4,14 @@
 #include "generator.h"
 
 int main(const int argc, char *argv[]) {
-    Args args = process_args(argc, argv);
+    auto [input_file, output_file] = process_args(argc, argv);
 
-    if(!validate_file(args.input_file)) {
-        std::cout << args.input_file << " is not a valid file." << std::endl;
+    if(!validate_file(input_file)) {
+        std::cout << input_file << " is not a valid file." << std::endl;
         exit(EXIT_FAILURE);
     }
 
-    const Lexer lexer(args.input_file);
+    const Lexer lexer(input_file);
     const auto tokens = lexer.tokenize();
 
     Generator generator(tokens);
@@ -19,7 +19,7 @@ int main(const int argc, char *argv[]) {
 
     std::cout << content << std::endl;
 
-    create_html_file(content, args.output_file);
+    create_html_file(content, output_file);
 
     return EXIT_SUCCESS;
 }
