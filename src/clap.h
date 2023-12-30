e2c07d118c157b1683f62db7ab379701cbfcb412
Hammad Majid <hammadmajid@proton.me>
2023-12-30 19:07:25 +0500

    docs: add code documentation

--- src/clap.h
+++ src/clap.h
@@ -1,19 +1,31 @@
 #ifndef CLAP_H
 #define CLAP_H
+
 #include <iostream>
 #include <string>
 #include <filesystem>
 #include <vector>
 
+/**
+ * @struct Args
+ * @brief Structure to store input and output file paths.
+ */
 struct Args {
-    std::string input_file;
-    std::string output_file;
+    std::string input_file; ///< Path to the input Markdown file.
+    std::string output_file; ///< Path to the output HTML file.
 };
 
+/**
+ * @class Clap
+ * @brief Command Line Argument Processor for converting .md files to .html.
+ */
 class Clap {
-    size_t m_argc;
-    std::vector<std::string> m_argv;
+    size_t m_argc; ///< Number of command line arguments.
+    std::vector<std::string> m_argv; ///< Vector to store command line arguments.
 
+    /**
+     * @brief Prints help information for using the program.
+     */
     static auto print_help() {
         std::cout << "Convert .md files to .html ones" << std::endl;
         std::cout << std::endl;
@@ -25,18 +37,34 @@ class Clap {
         std::cout << "\t\t--version | -v     Print version info" << std::endl;
     }
 
-    static auto has_extention(const std::string &path, const std::string &extension) -> bool {
+    /**
+     * @brief Checks if a file has a specific extension.
+     * @param path The path to the file.
+     * @param extension The expected file extension.
+     * @return True if the file has the specified extension, false otherwise.
+     */
+    static auto has_extension(const std::string &path, const std::string &extension) -> bool {
         return std::filesystem::exists(path) && std::filesystem::path(path).extension() == extension;
     }
+
 public:
-    Clap (const int argc, char * argv[]) : m_argc(argc) {
+    /**
+     * @brief Constructor for Clap class.
+     * @param argc Number of command line arguments.
+     * @param argv Array of command line arguments.
+     */
+    Clap(const int argc, char *argv[]) : m_argc(argc) {
         for (int i = 0; i < m_argc; i++) {
             m_argv.emplace_back(argv[i]);
         }
     }
 
+    /**
+     * @brief Processes command line arguments and returns Args structure.
+     * @return Args structure containing input and output file paths.
+     */
     [[nodiscard]] auto process_args() const -> Args {
-        for (auto &arg: m_argv) {
+        for (const auto &arg: m_argv) {
             if (arg == "--help" || arg == "-h") {
                 print_help();
                 exit(EXIT_SUCCESS);
@@ -53,19 +81,19 @@ public:
             exit(EXIT_FAILURE);
         }
 
-        if (!has_extention(m_argv.at(1), ".md")) {
+        if (!has_extension(m_argv.at(1), ".md")) {
             std::cout << "Expected " << m_argv.at(1) <<
-                " to be a .md file. See help with --help option." << std::endl;
+                    " to be a .md file. See help with --help option." << std::endl;
             exit(EXIT_FAILURE);
         }
 
-        if (!has_extention(m_argv.at(2), ".html")) {
+        if (!has_extension(m_argv.at(2), ".html")) {
             std::cout << "Expected " << m_argv.at(2) <<
-                " to be a .html file. See help with --help option." << std::endl;
+                    " to be a .html file. See help with --help option." << std::endl;
             exit(EXIT_FAILURE);
         }
 
-        return Args {
+        return Args{
             .input_file = m_argv[1],
             .output_file = m_argv[2]
         };
